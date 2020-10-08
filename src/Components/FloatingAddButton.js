5a435f3c873ad2178183a3d0a7f0d8fe4a9fa89e
Rashi Kulshrestha <rashikul5.rk@gmail.com>
2020-10-08 16:29:25 +0530

    Add features

---
+++ src/Components/FloatingAddButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FloatingAddButton = ({ onClick }) => {
+  return (
+    <div className="add-button">
+      <button onClick={() => onClick()}>&#x271A;</button>
+    </div>
+  );
+};
+export default FloatingAddButton;
